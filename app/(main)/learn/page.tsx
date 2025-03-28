7f31eb35c56b237e9564b7014bf5e3fbae43c9b2
Dimitar Mitev <d.mitev920123@gmail.com>
2025-03-28 17:37:33 +0200

    feat: units for learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,12 +2,16 @@ import { Header } from "./header";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -24,6 +28,12 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <h2 className="font-bold text-lg">{unit.title}</h2>
+            <p className="mt-2">{unit.description}</p>
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
