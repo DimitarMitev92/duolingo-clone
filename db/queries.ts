7b6de831a2f0b0d09bc6a591f11448ec96d0d4ae
Dimitar Mitev <d.mitev920123@gmail.com>
2025-03-28 17:33:33 +0200

    feat: getUnits query

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { eq } from "drizzle-orm";
-import { userProgress, courses } from "./schema";
+import { userProgress, courses, units } from "./schema";
 import { auth } from "@clerk/nextjs/server";
 
 export const getUserProgress = cache(async () => {
@@ -15,6 +15,52 @@ export const getUserProgress = cache(async () => {
   });
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  try {
+    const userProgress = await getUserProgress();
+
+    if (!userProgress?.activeCourseId) return [];
+
+    const data = await db.query.units.findMany({
+      where: eq(units.courseId, userProgress.activeCourseId),
+      with: {
+        lessons: {
+          with: {
+            challenges: {
+              with: {
+                challengeProgress: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const normalizedData = data.map((unit) => {
+      const lessonWithCompletedStatus = unit.lessons.map((lesson) => {
+        const allCompletedChallenges = lesson.challenges.every((challenge) => {
+          return (
+            challenge.challengeProgress &&
+            challenge.challengeProgress.length > 0 &&
+            challenge.challengeProgress.every(
+              (challengeProgress) => challengeProgress.completed
+            )
+          );
+        });
+
+        return { ...lesson, completed: allCompletedChallenges };
+      });
+
+      return { ...unit, lessons: lessonWithCompletedStatus };
+    });
+
+    return normalizedData;
+  } catch (error) {
+    console.error("Error fetching units:", error);
+    return [];
+  }
 });
 
 export const getCourses = cache(async () => {
