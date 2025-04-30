0cdf4f0d96585d7c2950b3e94426d85280d31b28
Dimitar Mitev <d.mitev920123@gmail.com>
2025-04-30 13:25:42 +0300

    feat: admin challenge options crud functionality

---
+++ app/admin/challengeOption/create.tsx
@@ -0,0 +1,21 @@
+import {
+  Create,
+  ReferenceInput,
+  SimpleForm,
+  TextInput,
+  BooleanInput,
+} from "react-admin";
+
+export const ChallengeOptionCreate = () => {
+  return (
+    <Create>
+      <SimpleForm>
+        <TextInput source="text" label="Text" />
+        <BooleanInput source="correct" label="Correct option" />
+        <ReferenceInput source="challengeId" reference="challenges" />
+        <TextInput source="imageSrc" label="Image URL" />
+        <TextInput source="audioSrc" label="Audio URL" />
+      </SimpleForm>
+    </Create>
+  );
+};
